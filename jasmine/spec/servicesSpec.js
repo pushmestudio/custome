625c9a134697ed8e819988bf726a80f41a8167d4
tkhm <devtkhm@gmail.com>
2015-05-10 17:07:54 +0900

    追加 Jasmineによるテストのセットアップ完了 refs #81
    SpecRunner.htmlを見れば結果が参照可能
    書き方などについては後日更新するが、dbConnectorSpecを見ればわかるはず

---
+++ jasmine/spec/servicesSpec.js
@@ -0,0 +1,14 @@
+﻿describe('servicesのテスト', function() {
+  var scope;
+  beforeEach(module('mainApp.services'));
+
+  describe('getのテスト', function() {
+    beforeEach(inject(function(Boards) {
+      scope = Boards;
+    }));
+    
+    it('boardId:0のname"タスクボード1"が返る', function() {
+      expect(scope.get(0).name).toEqual('タスクボード1');
+    });
+  });
+});
