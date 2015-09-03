4d6b3d44a6707e3c622a03dcde8a0c17125ee83f
capsmalt <capsmalt@gmail.com>
2015-09-03 19:19:18 +0900

    Camera API利用サンプルを生成

--- www/js/services.js
+++ www/js/services.js
@@ -417,3 +417,21 @@ angular.module('mainApp.services', ['mainApp.dbConnector'])
     }
   };
 })
+
+.factory('Camera', ['$q', function($q) {
+
+  return {
+    getPicture: function(options) {
+      var q = $q.defer();
+
+      navigator.camera.getPicture(function(result) {
+        // Do any magic you need
+        q.resolve(result);
+      }, function(err) {
+        q.reject(err);
+      }, options);
+
+      return q.promise;
+    }
+  }
+}])
