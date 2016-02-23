c683b66ed1392ed5a9aa1056c1f27114a77ec3aa
tkhm <no-reply@tkhm.dev>
2016-02-23 19:24:58 +0900

    Try the directive ver (failed)

--- www/js/directives.js
+++ www/js/directives.js
@@ -50,3 +50,17 @@ angular.module('mainApp.directives', [])
   }
 })
 
+.directive('focusMe', function($timeout) {
+    return {
+        link: function(scope, element, attrs) {
+            console.log("focusMe directive init");
+            if(scope.focusMe ) {
+                $timeout(function() {
+                    console.log(" adding focus to element")
+
+                    element[0].focus();
+                }, 150);
+            }
+        }
+    };
+})
