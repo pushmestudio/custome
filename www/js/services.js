ce50961cffedd807ae6bd538283a76185aa963ef
tkhm <devtkhm@gmail.com>
2016-03-08 19:01:28 +0900

    Update logging module

--- www/js/services.js
+++ www/js/services.js
@@ -904,7 +904,7 @@ angular.module('mainApp.services', ['mainApp.dbConnector', 'ngCordova'])
 /**
  * @module d
  * @description ログ出力モジュール DEBUG_MODE ON時にログを出力させる、値の設定はapp.jsにて
- * ログ出力呼び出し時の簡便さを優先するため、モジュール名はdebugの'd'
+ * ログ出力呼び出し時の簡便さを優先するため、モジュール名はdebuggerの'd'
  * @requires $rootScope
  */
 .factory('d', function($rootScope) {
@@ -916,17 +916,36 @@ angular.module('mainApp.services', ['mainApp.dbConnector', 'ngCordova'])
    * @see http://flabo.io/code/20140926/01-angularjs-application-7-tips/
    * @see http://d.hatena.ne.jp/hokaccha/20111216/1324026093
    */
+  var printLog;
+
+  /**
+   * @function debug
+   * @description DEBUG_MODEがtrueならd.debug()でデバッグログ出力、falseなら出力なし
+   */
   var printDebug;
+
+  /**
+   * @function trace
+   * @description DEBUG_MODEがtrueならd.trace()で呼び出し元などを辿って出力、falseなら出力なし
+   */
+  var printTrace;
+
   (function() {
     if(DEBUG_MODE) {
+      printLog = console.log.bind(console); // console.logの処理をバインド
       printDebug = console.debug.bind(console); // console.debugの処理をバインド
+      printTrace = console.trace.bind(console); // console.traceの処理をバインド
     } else {
-      printDebug = function(){}; // debugMode = falseのときは何も出力しない
+      printLog = function(){}; // debugMode = falseのときは何も出力しない
+      printDebug = function(){};
+      printTrace = function(){};
     }
   })();
 
   return {
-    log: printDebug
+    log: printLog,
+    debug: printDebug,
+    trace: printTrace
   };
 })
 
