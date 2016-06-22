4bac26cf909d8f7b52ece2f26f73d2684a7d9bfd
tkhm <devtkhm@gmail.com>
2016-06-22 21:48:27 +0900

    Add intro-js(experimental) refs #196

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -3,7 +3,7 @@
  * ビューとモデルをつなぐ各種コントローラを定義している
  * @copyright (c) 2015 PushMe Studio
  */
-angular.module('mainApp.controllers', ['mainApp.services', 'mainApp.directives', 'toaster', 'ngAnimate'])
+angular.module('mainApp.controllers', ['mainApp.services', 'mainApp.directives', 'toaster', 'ngAnimate', 'angular-intro'])
 
 /**
  * @module BoardsCtrl
@@ -55,6 +55,70 @@ angular.module('mainApp.controllers', ['mainApp.services', 'mainApp.directives',
   // テンプレート一覧を読み込む
   $scope.templates = Boards.getAllTemplates();
   $scope.listCanSwipe = true; // リストに対してスワイプ操作を可能にする
+
+  $scope.CompletedEvent = function (scope) {
+    console.log("Completed Event called");
+  };
+
+  $scope.ExitEvent = function (scope) {
+    console.log("Exit Event called");
+  };
+
+  $scope.ChangeEvent = function (targetElement, scope) {
+    console.log("Change Event called");
+    console.log(targetElement);  //The target element
+    console.log(this);  //The IntroJS object
+  };
+
+  $scope.BeforeChangeEvent = function (targetElement, scope) {
+    console.log("Before Change Event called");
+    console.log(targetElement);
+  };
+
+  $scope.AfterChangeEvent = function (targetElement, scope) {
+    console.log("After Change Event called");
+    console.log(targetElement);
+  };
+
+  $scope.IntroOptions = {
+    steps:[
+    {
+      element: document.querySelector('#step1'),
+      intro: "This is the first tooltip."
+    },
+    {
+      element: document.querySelectorAll('#step2')[0],
+      intro: "<strong>You</strong> can also <em>include</em> HTML",
+      position: 'right'
+    },
+      {
+        element: '#step3',
+        intro: 'More features, more fun.',
+        position: 'left'
+      },
+      {
+        element: '#step4',
+        intro: "Another step.",
+        position: 'bottom'
+      },
+        {
+          element: '#step5',
+          intro: 'Get it, use it.'
+        }
+    ],
+    showStepNumbers: false,
+    exitOnOverlayClick: true,
+    exitOnEsc:true,
+    nextLabel: '<strong>NEXT!</strong>',
+    prevLabel: '<span style="color:green">Previous</span>',
+    skipLabel: 'Exit',
+    doneLabel: 'Thanks'
+  };
+  $scope.ShouldAutoStart = false;
+
+  $scope.CallMe = function() {
+    d.log('hello');
+  }
 
   /**
    * @function selectWallpaper
